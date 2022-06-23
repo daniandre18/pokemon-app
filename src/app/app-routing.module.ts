2fcb8bd68f5f5c01ced53c73c5fd05ce9d21422d
Danilo <daniandre18@gmail.com>
2022-06-22 19:17:52 -0500

    servicio y componente

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PokemonComponent } from './components/pokemon/pokemon.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: PokemonComponent },
+  { path: '**', component: PokemonComponent }
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/models/pokemon.ts
@@ -0,0 +1,12 @@
+export interface Pokemon {
+
+    id:number,
+    name:string,
+    image:string,
+    attack:number,
+    defense:number,
+    hp:number,
+    type:string,
+    id_author:number
+}
+
